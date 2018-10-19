59fe02b25a04c4b87004ddb4ccfdefce22badf1c
solomahina <solomahina19@gmail.com>
2018-10-19 14:04:40 +0400

    Added entry point

--- src/com/company/RPIS61/Solomahina/wdad/Application.java
+++ src/com/company/RPIS61/Solomahina/wdad/Application.java
@@ -2,6 +2,6 @@ package com.company.RPIS61.Solomahina.wdad;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("I'm Anastasia \"Kabelepletchik AutoVAZ\" Solomahina, and I'm not a monkey");
+        System.out.println("I'm Anastasia \"Kabelepletchik AutoVAZ\" Solomahina, and I'm not a monkey ");
     }
 }
